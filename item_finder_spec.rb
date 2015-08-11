7feafabc9645e727b086b5207fc073096c980d25
JayChetty <jaypaulchetty@gmail.com>
2015-08-11 19:33:42 +0100

    state expected behavior for unknown item

--- item_finder_spec.rb
+++ item_finder_spec.rb
@@ -11,4 +11,11 @@ describe ItemFinder do
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
     expect( item_finder.find_items( ["hats", "shoes"] ) ).to include( { "hats" => 0, "shoes"=> 1 } )
   end
+
+  it "should give location nil if item doesn not exist" do
+    item_finder = ItemFinder.new
+    item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
+    expect( item_finder.find_items( ["gloves", "shoes"] ) ).to include( { "gloves" => nil, "shoes"=> 1 } )
+  end
+
 end
