dee2f7eb4b8a94061d45875003cead3c7ec92832
JayChetty <jaypaulchetty@gmail.com>
2015-08-11 20:04:31 +0100

    typos

--- item_finder_spec.rb
+++ item_finder_spec.rb
@@ -24,7 +24,7 @@ describe ItemFinder do
     expect( item_finder.find_items( ["hats", "shoes"] ) ).to include( { "hats" => 0, "shoes"=> 1 } )
   end
 
-  it "should give location nil if item doesn not exist" do
+  it "should give location nil if item does not exist" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
     expect( item_finder.find_items( ["sausages", "shoes"] ) ).to include( { "sausages" => nil, "shoes"=> 1 } )
@@ -36,7 +36,7 @@ describe ItemFinder do
     expect( item_finder.distance( ["gloves", "shoes"] ) ).to equal(6)
   end
 
-  it "should return false if item is missing" do
+  it "should return false for distance if item is missing" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:1, name:"hats" }, { position:4, name:"shoes" }, { position:10, name:"gloves" } ] )
     expect( item_finder.distance( ["sausages", "shoes"] ) ).to equal(false)
