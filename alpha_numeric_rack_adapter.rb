6ed746e35cd09aaa88c0f6938cc70c4340f6e843
JayChetty <jaypaulchetty@gmail.com>
2015-08-11 22:01:40 +0100

    adapter takes care of alpha numeric language to locate indexes

--- alpha_numeric_rack_adapter.rb
+++ alpha_numeric_rack_adapter.rb
@@ -4,5 +4,50 @@ class AlphaNumericRackAdapter
     @number_of_racks = number_of_racks
     @rack_length = rack_length
     @inverted_racks = inverted_racks
+    @letter_array = ("A".."Z").to_a()
   end
+
+  def alpha_numeric_string_to_index(string_code)
+    rack_letter = string_code[0]
+    rack_number = @letter_array.find_index do |letter|
+      rack_letter == letter
+    end
+    position_on_rack = string_code[1..-1].to_i - 1
+    (rack_number * 10) + position_on_rack
+  end
+
+  def index_to_alpha_numeric_string(string_code)
+    rack_letter = string_code[0]
+    rack_number = @letter_array.find_index do |letter|
+      rack_letter == letter
+    end
+    position_on_rack = string_code[1..-1].to_i - 1
+    (rack_number * 10) + position_on_rack
+  end
+
+  def load_items(items_to_load)
+    items_to_load.each do |item|
+      item[:position] = alpha_numeric_string_to_index(item[:position])
+    end
+    @item_finder.load_items(items_to_load)
+  end
+
+  def search_locations_with_distance(locations)
+    indexs = locations.map do |location|
+      alpha_numeric_string_to_index(location)
+    end
+    @item_finder.search_locations_with_distance(indexs)
+  end
+
+  # def find_items_with_path(item_names)
+  #   indexed_values = @item_finder.find_items_with_path(item_names)
+  #   indexed_values.locations.each do |name, location|
+
+  #   end
+  # end
+
+
+
+  # def method_missing
+  # end
 end
--- alpha_numeric_rack_adapter_spec.rb
+++ alpha_numeric_rack_adapter_spec.rb
@@ -2,7 +2,41 @@ require_relative('./alpha_numeric_rack_adapter.rb')
 
 describe AlphaNumericRackAdapter do
   let(:item_finder){ double('finder') }
-  it "should be created with an item finder, number of racks, rack length, and inverted columns" do
-    adapter = AlphaNumericRackAdapter.new( item_finder, 3, 10, [0,2] )
+  let(:adapter){adapter = AlphaNumericRackAdapter.new( item_finder, 3, 10, [0,2] )}
+  # it "should be created with an item finder, number of racks, rack length, and inverted columns" do
+  #   adapter = AlphaNumericRackAdapter.new( item_finder, 3, 10, [0,2] )
+  # end
+
+  it "should convert alpha numeric strings to array indexes" do
+    expect( adapter.alpha_numeric_string_to_index("A1") ).to equal(0)
   end
+
+  it "should convert alpha numeric strings to array indexes" do
+    expect( adapter.alpha_numeric_string_to_index("B1") ).to equal(10)
+  end
+
+  it "should convert alpha numeric strings to array indexes" do
+    expect( adapter.alpha_numeric_string_to_index("C10") ).to equal(29)
+  end
+
+  # it "raise exeception if out of range" do
+  #   expect( adapter.alpha_numeric_string_to_index("A0") ).to equal(29)
+  # end
+
+  it "should provide interface to load items using alpha numeric language" do
+    expect( item_finder ).to receive(:load_items).with( [ { position:0, name:"hats" }, { position:10, name:"shoes" } ] )
+    adapter.load_items( [ { position:"A1", name:"hats" }, { position:"B1", name:"shoes" } ] )
+  end
+
+  it "should provide interface to search locations with distance using alpha numeric language" do
+    expect( item_finder ).to receive(:search_locations_with_distance).with( [ 0, 10 ] )
+    adapter.search_locations_with_distance( [ "A1", "B1"] )
+  end
+
+  # it "should provide interface to find items with path returned in alpha numeric language" do
+  #   item_finder.stub(:find_items_with_path).with(['gloves', 'shoes']) do 
+  #     { locations:{ "gloves" => 10, "shoes"=> 4 }, path: [ 4, 10 ] }
+  #   end
+  #   expect( adapter.find_items_with_path( ["gloves", "shoes"] ) ).to include( locations:{ "gloves" => "B1", "shoes"=> "A3" }, path: [ "A3", "B1" ] )
+  # end
 end
