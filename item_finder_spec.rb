c9b4f600b2f4a1a89cb6b202247a034c2163b097
JayChetty <jaypaulchetty@gmail.com>
2015-08-12 13:55:58 +0100

    improve item finder spec to check object equivelence

--- item_finder_spec.rb
+++ item_finder_spec.rb
@@ -15,42 +15,42 @@ describe ItemFinder do
   it "should assign nil if no item at location" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
-    expect(item_finder.search_locations( [ 0, 2 ] ) ).to include( { 0 => "hats", 2 => nil } )
+    expect(item_finder.search_locations( [ 0, 2 ] ) ).to eq( { 0 => "hats", 2 => nil } )
   end
 
   it "should reveal items for a list of locations and show distance between them" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
-    expect(item_finder.seach_locations_with_distance( [ 0, 1 ] ) ).to include( items: { 0 => "hats", 1 => "shoes" }, distance: 1 )
+    expect(item_finder.seach_locations_with_distance( [ 0, 1 ] ) ).to eq( items: { 0 => "hats", 1 => "shoes" }, distance: 1 )
   end
 
   it "should still show distance value even when item doesn't exist in location " do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
-    expect(item_finder.seach_locations_with_distance( [ 0, 2 ] ) ).to include( items: { 0 => "hats", 2 => nil }, distance: 2 )
+    expect(item_finder.seach_locations_with_distance( [ 0, 2 ] ) ).to eq( items: { 0 => "hats", 2 => nil }, distance: 2 )
   end
 
   it "should find multiple items" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
-    expect( item_finder.find_items( ["hats", "shoes"] ) ).to include( { "hats" => 0, "shoes"=> 1 } )
+    expect( item_finder.find_items( ["hats", "shoes"] ) ).to eq( { "hats" => 0, "shoes"=> 1 } )
   end
 
   it "should give location nil if item does not exist" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:0, name:"hats" }, { position:1, name:"shoes" } ] )
-    expect( item_finder.find_items( ["sausages", "shoes"] ) ).to include( { "sausages" => nil, "shoes"=> 1 } )
+    expect( item_finder.find_items( ["sausages", "shoes"] ) ).to eq( { "sausages" => nil, "shoes"=> 1 } )
   end
 
   it "should find items and show the path required to retrieve items" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:1, name:"hats" }, { position:4, name:"shoes" }, { position:10, name:"gloves" } ] )
-    expect( item_finder.find_items_with_path( ["gloves", "shoes"] ) ).to include( locations:{ "gloves" => 10, "shoes"=> 4 }, path: [ 4, 10 ] )
+    expect( item_finder.find_items_with_path( ["gloves", "shoes"] ) ).to eq( locations:{ "gloves" => 10, "shoes"=> 4 }, path: [ 4, 10 ] )
   end
 
   it "should return false for path if item cannot be found" do
     item_finder = ItemFinder.new
     item_finder.load_items( [ { position:1, name:"hats" }, { position:4, name:"shoes" }, { position:10, name:"gloves" } ] )
-    expect( item_finder.find_items_with_path( ["sausages", "shoes"] ) ).to include( locations:{ "sausages" => nil, "shoes"=> 4 }, path: false)
+    expect( item_finder.find_items_with_path( ["sausages", "shoes"] ) ).to eq( locations:{ "sausages" => nil, "shoes"=> 4 }, path: false)
   end
 end
